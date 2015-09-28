9f2c9627b9aba4f46ff3c881eb3fb4b4fc3290ad
mdluo <mdluo@nsmss.com>
2015-09-28 16:07:26 +0800

    Update git command for deployment

--- github-webhook-handler.php
+++ github-webhook-handler.php
@@ -14,7 +14,7 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
   if (strcmp($signature, $hash) == 0) {
-    echo shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
+    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
     exit();
   }
 }

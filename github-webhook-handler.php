e287141d919f58d3862ac16097bca79c82224803
liutim <84539606@qq.com>
2016-06-22 13:07:41 +0800

    添加分支名属性,只有这个分之下的push才导致更新: $branchname 并且直接使用git pull 默认当然是merge。 因为在服务器上的更改一般只有数据库链接字符串。

--- github-webhook-handler.php
+++ github-webhook-handler.php
@@ -8,14 +8,22 @@ $secret = "";
 // e.g. "/var/www/respostory"
 $path = "";
 
+//branch name which pushed will trick the server upgrde.
+//e.g. test or product.
+
+$branchname = "master";
+
 // Headers deliveried from GitHub
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
 
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
   if (strcmp($signature, $hash) == 0) {
-    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-    exit();
+    if(strpos($HTTP_RAW_POST_DATA,'"refs/heads/'.$branchname)>0){
+      echo shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
+      exit();
+    }
   }
 }
 
